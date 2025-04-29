77cadb39eb223f18c551a0c66941424287e7848f
Noah <noahtevans@yahoo.com>
2025-04-28 23:04:22 -0600

    adjusted BufferReader and insert method

--- src/BTree.java
+++ src/BTree.java
@@ -5,51 +5,59 @@
 
 /**
  *
- * @author rocky
+ * @author NoahTEvans
  */
 public class BTree<T extends Comparable<T>>
 {
-    private class BTNode {
-        
-        BTNode leftNode;
-        BTNode rightNode;
-        Comparable info;
-        
-        BTNode(Comparable info) {
+
+    private class BTNode
+    {
+
+        private BTNode leftNode;
+        private BTNode rightNode;
+        private Comparable info;
+
+        BTNode(Comparable info)
+        {
             this.info = info;
         }
-    
-        public void insert(BTNode node) {
-            switch (this.info.compareTo(node)) {
-                case -1, 0:
-                    if (leftNode != null) {
-                        leftNode.insert(node);
-                    } else {
-                        leftNode = node;
-                    }
-                    break;
-                case 1:
-                    if (rightNode != null) {
-                        rightNode.insert(node);
-                    } else {
-                        rightNode = node;
-                    }
-                    break;
-                default:
-                    break;
+
+        public void insert(BTNode node)
+        {
+            if (node.info.compareTo(this.info) <= 0)
+            { // go left
+                if (leftNode != null)
+                {
+                    leftNode.insert(node);
+                } else
+                {
+                    leftNode = node;
+                }
+            } else
+            { // go right
+                if (rightNode != null)
+                {
+                    rightNode.insert(node);
+                } else
+                {
+                    rightNode = node;
+                }
             }
         }
     }
-    
+
     BTNode root;
-    
-    public void insert(T info) {
+
+    public void insert(T info)
+    {
         BTNode node = new BTNode(info);
-        if (root == null) {
+        if (root == null)
+        {
             root = node;
-        } else {
+        } else
+        {
             root.insert(node);
         }
     }
-    
+
 }
--- src/BTreeMain.java
+++ src/BTreeMain.java
@@ -9,41 +9,44 @@ import java.util.List;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author justinsnider
  */
-public class BTreeMain {
+public class BTreeMain
+{
 
     /**
      * @param args the command line arguments
+     * "C:\Users\rocky\OneDrive\Desktop\BTree Data File.txt"
      */
-    public static void main(String[] args) {
-        
-        try {
-            BufferedReader br = new BufferedReader(new FileReader("/Users/justinsnider/Desktop/BTreeDataFile.txt"));
+    public static void main(String[] args)
+    {
+
+        try
+        {
+            BufferedReader br = new BufferedReader(new FileReader("C:/Users/rocky/OneDrive/Desktop/BTree Data File.txt"));
             StringBuilder sb = new StringBuilder();
             String line = br.readLine();
             List<String> lines = new ArrayList<String>();
 
-            while (line != null) {
-                sb.append(line);
-                sb.append(System.lineSeparator());
-                line = br.readLine();
-                lines.add(line);
+            while (line != null)
+            {
+                lines.add(line);   // add first
+                line = br.readLine(); // then read next
             }
-            
+
             BTree tree = new BTree();
-            
-            for(int i = 0; i < lines.size(); i++) {
+
+            for (int i = 0; i < lines.size(); i++)
+            {
                 String animal = lines.get(i);
                 System.out.println(animal);
                 tree.insert(animal);
             }
-            
-            
-        } catch(Exception ex) {
+
+        } catch (Exception ex)
+        {
             System.out.println(ex);
         }
     }
